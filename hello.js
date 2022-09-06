0a69b0ca44af78af7e23e12659ac3db82b7a55f7
sonuengineer <63283798+sonuengineer@users.noreply.github.com>
2022-09-06 18:54:52 +0530

    Update hello.js
    Based on the url the user hits , I want you to return custom responses.
    When url = /home , return response ==> Welcome home
    When url = /about, return response ==> Welcome to About Us page
    When url =/node, return response ==> Welcome to my Node Js project

--- hello.js
+++ hello.js
@@ -1,1 +1,36 @@
 console.log("hello World");
+var http = require('http'); // Import Node.js core module
+
+var server = http.createServer(function (req, res) {   //create web server
+    if (req.url == '/') { //check the URL of the current request
+        
+        // set response header
+        res.writeHead(200, { 'Content-Type': 'text/html' }); 
+        
+        // set response content    
+        res.write('<html><body><p>Welcome home Page.</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/about") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p> Welcome to About Us page</p></body></html>');
+        res.end();
+    
+    }
+    else if (req.url == "/node") {
+        
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>Welcome to my Node Js project.</p></body></html>');
+        res.end();
+    
+    }
+    else
+        res.end('Invalid Request!');
+
+});
+
+server.listen(5000); //6 - listen for any incoming requests
+
+console.log('Node.js web server at port 5000 is running..')
